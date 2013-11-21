c10e03cd71cd564e70fb6b350e3f40dbbc112453
ceceiliaallwein <ceceilia.allwein@gmail.com>
2013-11-20 23:46:56 -0500

    sprint 2 commits

--- my_first_app/my_first_app.py
+++ my_first_app/my_first_app.py
@@ -1,13 +1,15 @@
 
 import flask, flask.views
 import os
+import functools
 
 app = flask.Flask(__name__)
 
 # Don't do this! 
 app.secret_key = "bacon"
 
-users = {'ceceilia':'bacon'}
+# this is your database; ceceilia is the key, pw is bacon
+users = {'ceceilia':'boom'}
 
 class Main(flask.views.MethodView): 
 	def get(self):
@@ -15,51 +17,70 @@ class Main(flask.views.MethodView):
 
 	def post(self):
 		if 'logout' in flask.request.form:
-            flask.session.pop('username', None)
-            return flask.redirect(flask.url_for('index'))
-        required = ['username', 'passwd']
-        for r in required:
-            if r not in flask.request.form:
-                flask.flash("Error: {0} is required.".format(r))
-                return flask.redirect(flask.url_for('index'))
-        username = flask.request.form['username']
-        passwd = flask.request.form['passwd']
-        if username in users and users[username] == passwd:
-            flask.session['username'] = username
-        else:
-            flask.flash("Username doesn't exist or incorrect password")
-        return flask.redirect(flask.url_for('index'))
+			# pops the username off the session
+			# what's the second arguement? password? built-in?  
+			flask.session.pop('username', None)
+			return flask.redirect(flask.url_for('index'))
+		required = ['username','password']
+		for r in required:
+			if r not in flask.request.form:
+				flask.flash("Error: {0} is required.".format(r))
+				# preferred method for sending user to a different path
+				return flask.redirect(flask.url_for('index'))
+		
+		username = flask.request.form['username']
+		password = flask.request.form['password']
+		# if username is in users (database) and 
+		# user at that username has the given (passed through) password 
+		if username in users and users[username] == password:
+			# if they're logged on then we can store their 
+			# username in the session 
+			flask.session['username'] = username
+		else:
+			flask.flash("Username doesn't exist or incorrect password")
+		return flask.redirect(flask.url_for('index'))
 
-	def login_required(method):
-   		@functools.wraps(method)
-    	def wrapper(*args, **kwargs):
-        	if 'username' in flask.session:
-            	return method(*args, **kwargs)
-        	else:
-            	flask.flash("A login is required to see the page!")
-            	return flask.redirect(flask.url_for('index'))
-    	return wrapper
+ 
+
+def login_required(method):
+	# decorator
+	@functools.wraps(method)
+	# takes args and keyword args
+	def wrapper(*args, **kwargs):
+		if 'username' in flask.session:
+			return method(*args, **kwargs)
+		else:
+			flask.flash("A login is required to see the page!")
+			return flask.redirect(flask.url_for('index'))
+	return wrapper
 
 
 class Remote(flask.views.MethodView):
+	# decorator replaces get or post with the wrapper
+	# but the wrapper also knows what the get or post
+	# methods are
+	# the decorator executes, then it invokes the 
+	# method via *args (the method) and **kwargs (what's
+	# passed through the methods)
+
 	@login_required
-    def get(self):
-        return flask.render_template('remote.html')
-        
-    @login_required
-    def post(self):
-        result = eval(flask.request.form['expression'])
-        flask.flash(result)
-        return flask.redirect(flask.url_for('remote'))
+	def get(self):
+		return flask.render_template('remote.html')
+		
+	@login_required
+	def post(self):
+		result = eval(flask.request.form['expression'])
+		flask.flash(result)
+		return flask.redirect(flask.url_for('remote'))
 
+		#alternate method? 
+		#return self.get()
 
-app.add_url_rule('/', 
-				view_func=View.as_view('main'), 
-				methods=('GET', 'POST'))
-
-app.add_url_rule('/Remote',
-				view_func=View.as_view('remote'),
-				methods=('GET', 'POST'))
+# had View.as_view instead of Main.as_view for a while
+# must specify the class as a view 
+# clarify what this is actually doing? 
+app.add_url_rule('/',view_func=Main.as_view('main'),methods=('GET', 'POST'))
+app.add_url_rule('/Remote',view_func=Remote.as_view('remote'),methods=('GET', 'POST'))
 
 app.debug = True
 app.run()
